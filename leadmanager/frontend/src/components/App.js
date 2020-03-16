2e00a73f0c3cb103fd66ae09186f5954ca5c39ed
SofianSaleh <sofiane.saleh11@egmail.com>
2020-03-16 19:13:53 +0100

    implementing redux

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -2,16 +2,21 @@ import React, { Component, Fragment } from 'react'
 import ReactDom from "react-dom";
 import Header from './layout/header.js'
 import Dashboard from './leads/dashboard'
+import { Provider } from 'react-redux'
+import { store } from '../store'
 
 class App extends Component {
     render() {
         return (
+            <Provider store={store}>
+
             <Fragment>
                 <Header />
                 <div class="container">
                     <Dashboard />
                 </div>
             </Fragment>
+            </Provider>
         )
     }
 }
