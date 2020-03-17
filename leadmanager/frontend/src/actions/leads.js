20d4fe4758fb24b031c8adbc9ea2c5999ee7c798
SofianSaleh <sofiane.saleh11@egmail.com>
2020-03-17 21:44:24 +0200

    finished error handling

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { GET_LEADS, DELETE_LEAD, ADD_LEAD, GET_ERRORS } from "./types";
-
+import { createMessage } from "./messages";
 // Get Leads
 
 export const getLeads = () => dispatch => {
@@ -20,24 +20,17 @@ export const deleteLead = id => dispatch => {
   axios
     .delete(`/api/leads/${id}/`)
     .then(res => {
+      dispatch(createMessage({ deleteLead: "Lead Deleted" }));
       dispatch({ type: DELETE_LEAD, payload: id });
     })
-    .catch(e => {
-      //   const error = {
-      //     msg: err.response.data,
-      //     msg: err.response.status
-      //   };
-      //   dispatch({
-      //     type: GET_ERRORS,
-      //     payload: error
-      //   });
-    });
+    .catch(e => {});
 };
 
 export const addLead = lead => dispatch => {
   axios
     .post(`/api/leads/`, lead)
     .then(res => {
+      dispatch(createMessage({ addLead: "Lead Added" }));
       dispatch({ type: ADD_LEAD, payload: res.data });
     })
     .catch(e => {
--- leadmanager/frontend/src/components/layout/Alerts.js
+++ leadmanager/frontend/src/components/layout/Alerts.js
@@ -5,11 +5,20 @@ import PropTypes from "prop-types";
 
 export class Alerts extends Component {
   static propTypes = {
-    error: PropTypes.object.isRequired
+    error: PropTypes.object.isRequired,
+    message: PropTypes.object.isRequired
   };
   componentDidUpdate(prevProps) {
-    const { error, alert } = this.props;
-    if (error) {
+    const { error, alert, message } = this.props;
+    if (error !== prevProps.error) {
+      if (error.msg.name) alert.error(`Name: ${error.msg.name.join("")}`);
+      if (error.msg.email) alert.error(`Email: ${error.msg.email.join("")}`);
+      if (error.msg.message)
+        alert.error(`Message: ${error.msg.message.join("")}`);
+    }
+    if (message !== prevProps.message) {
+      if (message.deleteLead) alert.success(message.deleteLead);
+      if (message.addLead) alert.success(message.addLead);
     }
   }
   render() {
@@ -17,8 +26,9 @@ export class Alerts extends Component {
   }
 }
 
-const mapStateToProps = state => {
-  error: state.errors;
-};
+const mapStateToProps = state => ({
+  error: state.errors,
+  message: state.messages
+});
 
 export default connect(mapStateToProps)(withAlert()(Alerts));
