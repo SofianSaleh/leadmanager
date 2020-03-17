e1e4d75448cb67835e3a5a59ead50764b8e3d53b
SofianSaleh <sofiane.saleh11@egmail.com>
2020-03-17 20:17:36 +0200

    working with react alerts

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,20 +1,30 @@
 import React, { Component, Fragment } from "react";
 import ReactDom from "react-dom";
+
+import { Provider as AlertProvider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
+
 import Header from "./layout/header.js";
 import Dashboard from "./leads/dashboard";
+
 import { Provider } from "react-redux";
 import store from "../store";
+
+// Alert options
+const alertOptions = { timeout: 4000, postion: "top center" };
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Fragment>
-          <Header />
-          <div className="container">
-            <Dashboard />
-          </div>
-        </Fragment>
+        <AlertProvider template={AlertTemplate} {...alertOptions}>
+          <Fragment>
+            <Header />
+            <div className="container">
+              <Dashboard />
+            </div>
+          </Fragment>
+        </AlertProvider>
       </Provider>
     );
   }
