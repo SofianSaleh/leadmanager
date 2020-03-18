ccfef916df2589fa6fb102021ded39462078b7f3
SofianSaleh <sofiane.saleh11@egmail.com>
2020-03-18 15:16:36 +0200

    Implementing React Router

---
+++ leadmanager/frontend/src/components/accounts/Register.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class Register extends Component {
+    state = {
+        username = '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
